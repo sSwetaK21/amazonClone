a0a229c4df1becc8d3fcedf278db9b18207f4e2a
Shweta Kodam <shweta.kodam@ecotechservices.com>
2024-07-01 19:36:19 +0530

    details

--- src/components/Products/ProductsDetail.jsx/ProductDetail.jsx
+++ src/components/Products/ProductsDetail.jsx/ProductDetail.jsx
@@ -16,40 +16,43 @@ export default function ProductDetail() {
   return (
     <div>
       <Header />
+      <div className="mt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="productImg">
+                <img src={product.imageUrl} alt="product" />
+              </div>
+            </div>{" "}
+            <div className="col-md-6">
+              <div className="productInfo">
+                <h1 className="productTitle">{product.title}</h1>
+                <hr></hr>
+                <div className="productPrice d-flex     align-items: center;">
+                  <p className="price">₹ {product.price}</p>
+                  <p className="discountPrice"> -{product.discountPrice}%</p>
+                </div>
+                <div className="amazon d-flex">
+                  <div className="img1">
+                    <img src="../src/assets/images/prodslide.png" alt="Img" />
+                    <span>7 Days Replacement</span>
+                  </div>
+                  <div className="img1">
+                    <img src="../src/assets/images/prodslide2.png" alt="Img" />
+                    <span>Free Delivery</span>
+                  </div>
+                  <div className="img1">
+                    <img src="../src/assets/images/prodsldie3.png" alt="Img" />
+                    <span>1 year Warranty</span>
+                  </div>
+                </div>
 
-      <div className="row">
-        <div className="col-6">
-          <div className="productImg">
-            <img src={product.imageUrl} alt="product" />
-          </div>
-        </div>
-        <div className="col-6">
-          <div className="productInfo">
-            <h1 className="productTitle">{product.title}</h1>
-            <hr></hr>
-            <div className="productPrice">
-              <p>Price :{product.price}</p>
-              <p>Discount: - {product.discountPrice}%</p>
-            </div>
-            <div className="amazon">
-              <div className="img1">
-                <img src="../src/assets/images/prodslide.png" alt="Img" />
-                <span>7 Days Replacement</span>
+                <div className="qunatity">
+                  <button className="qbtn">+</button>
+                  <input type="text" />
+                  <button className="qbtn">-</button>
+                </div>
               </div>
-              <div className="img1">
-                <img src="../src/assets/images/prodslide2.png" alt="Img" />
-                <span>Free Delivery</span>
-              </div>
-              <div className="img1">
-                <img src="../src/assets/images/prodsldie3.png" alt="Img" />
-                <span>1 year Warranty</span>
-              </div>
-            </div>
-
-            <div className="qunatity">
-              <button className="qbtn">+</button>
-              <input type="text" />
-              <button className="qbtn">-</button>
             </div>
           </div>
         </div>
